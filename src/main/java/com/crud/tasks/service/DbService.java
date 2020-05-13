e9154cdc6d33f2965214e42ac4b064175b0ef2ff
Ready2112 <kzak.air@gmail.com>
2020-05-13 16:11:39 +0200

    correct task 19.2

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,12 +1,12 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Task;
+import com.crud.tasks.exception.TaskNotFoundException;
 import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class DbService {
@@ -17,7 +17,7 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Optional<Task> getTaskById(Long taskId){
-        return repository.findById(taskId);
+    public Task getTaskById(Long taskId){
+        return repository.findById(taskId).orElseThrow(TaskNotFoundException::new);
     }
 }
--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -1,13 +1,31 @@
 package com.crud.tasks;
 
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.service.DbService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TasksApplicationTests {
 
+	@Autowired
+	DbService dbService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testGetTaskById(){
+		//Given
+		Long id = 1L;
+		//When
+		Task task = dbService.getTaskById(id);
+		//Then
+		Assert.assertEquals("test",task.getTitle());
+		Assert.assertEquals("test1",task.getContent());
 	}
 
 }
