3d60d24d0e2106cfa13203d4602148961fe0708c
Ready2112 <kzak.air@gmail.com>
2020-05-29 19:30:43 +0200

    put sleep before logical condicion

--- showtasks.bat
+++ showtasks.bat
@@ -1,11 +1,11 @@
 call runcrud.bat
+sleep 20
 if ERRORLEVEL 0 goto getTasks
 echo.
 echo runcrud.bat has errors - breaking work
 goto fail
 
 :getTasks
-sleep 20
 set GET_TASKS=http://localhost:8080/crud/v1/task/getTasks
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" %GET_TASKS%
 echo.
