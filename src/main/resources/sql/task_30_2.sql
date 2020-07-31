7be470bc3bf2e632fe79834d4c09eaca87ae826b
Ready2112 <kzak.air@gmail.com>
2020-07-31 09:40:05 +0200

    correct

--- src/main/resources/sql/task_30_2.sql
+++ src/main/resources/sql/task_30_2.sql
@@ -6,16 +6,16 @@ DELIMITER $$
 
 create procedure UpdateBestsellers()
 begin
-	declare BDR_ID, BOOKSREAD int;
+	declare READ_BOOK_ID, BOOKS_READ int;
 	declare FINISHED int default 0;
 	declare ALL_BOOKS cursor for select BOOK_ID from BOOKS;
     declare continue handler for not found set FINISHED = 1;
     open ALL_BOOKS;
     while (FINISHED = 0) do
-		fetch ALL_BOOKS into BDR_ID;
-        select count(*) from rents where BOOK_ID = BDR_ID into BOOKSREAD;
-		if(BOOKSREAD > 2) then
-				update books set BESTSELLER = true where BOOK_ID = BDR_ID;
+		fetch ALL_BOOKS into READ_BOOK_ID;
+        select count(*) from rents where BOOK_ID = READ_BOOK_ID into BOOKS_READ;
+		if(BOOKS_READ > 2) then
+				update books set BESTSELLER = true where BOOK_ID = READ_BOOK_ID;
 				commit;
 		end if;
     end while;
